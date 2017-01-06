24a5ee4c576a00232d50ed1c7e521a701013dc9a
guojielun <guojielun@masget.com>
2017-01-06 17:06:20 +0800

    '博客练习'

---
+++ j-blog/models/posts.js
@@ -0,0 +1,10 @@
+var marked = require('marked');
+var Post = require('../lib/mongo').Post;
+
+
+module.exports = {
+    //创建一篇文章
+    create:function create(post){
+        return Post.create(post).exec();
+    }
+};
